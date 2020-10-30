8ee2458c629fb68d9b4c2f3ee85a3230aaf5937e
buhi <yinshang1992@gmail.com>
2020-10-30 17:34:05 +0800

    Update seed.js
    从串行改并行

--- src/seed.js
+++ src/seed.js
@@ -1,10 +1,14 @@
-export default async function seed(dexieConnection, that) {
+export default function seed(_dexieConnection, that) {
   if (localStorage.getItem("seeded")) return;
   else localStorage.setItem("seeded", 1);
 
+  let promises = [];
   for (const modelName in that.models) {
     const model = that.models[modelName];
     if (!("seeding" in model)) continue;
-    await model.seeding(that.con[model.name]);
+    const promise = model.seeding(that.con[model.name]);
+    promises.push(promise);
   }
+  
+  return Promise.all(promises);
 }
