2f5a94440e7e2babeb5b891b917bef8e6ec7d1e1
akirarika <i@rika.ink>
2020-12-23 18:48:56 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/bash
 
 # 生成静态文件
-npm run docs:build
+npm run build
 
 # 进入生成的文件夹
-cd docs/.vuepress/dist
-
-git add -A
-git commit -m 'deploy'
-
-git push -f git@github.com:akirarika/kurimudb.git master:gh-pages
+cd docs/.vuepress/dist \
+&& git add -A \
+&& git commit -m 'deploy' \
+&& git push -f git@github.com:akirarika/kurimudb.git master:gh-pages
 
 cd -
