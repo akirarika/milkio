1b52892081c5a9c11866912c1d718c6e4bb34286
akirarika <i@rika.ink>
2020-12-23 18:56:22 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@
 npm run build
 
 # 进入生成的文件夹
-cd docs/.vuepress/dist \
-&& git add -A \
-&& git commit -m 'deploy' \
-&& git push -f git@github.com:akirarika/kurimudb.git master:gh-pages
+cd docs/.vuepress/dist
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:akirarika/kurimudb.git master:gh-pages
 
 cd -
