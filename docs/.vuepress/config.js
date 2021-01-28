efc277604504dcc63f9b5eb463f0bb96cf031618
akirarika <akirarika@users.noreply.github.com>
2021-01-28 10:15:06 +0800

    修改文档，修改发布脚本

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,13 @@ module.exports = {
   title: "Kurimudb",
   base: "/kurimudb/",
   themeConfig: {
-    nav: [{ text: "Github", link: "https://github.com/akirarika/kurimudb" }],
+    nav: [
+      { text: "Github", link: "https://github.com/akirarika/kurimudb" },
+      {
+        text: "更新日志",
+        link: "https://github.com/akirarika/kurimudb/releases",
+      },
+    ],
     sidebar: [
       ["/", "介绍"],
       ["/intro/", "入门"],
--- publish.sh
+++ publish.sh
@@ -2,10 +2,16 @@
 
 set -e
 
+read -p "You need to publish the NPM package? [y/N]" yn
+
 # 生成静态文件
 npm run ts:build
 npm run doc:build
-npm publish
+
+if [ "$yn" = "y" ]
+then
+  npm publish
+fi
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
