af28b043e6e0fd496c8cf0392e651d25417dec52
akirarika <akirarika@users.noreply.github.com>
2021-03-09 15:49:42 +0800

    船新版本⚓

--- dist/Connection/index.d.ts
+++
@@ -1,6 +0,0 @@
-import Dexie from "dexie";
-export default class Connection {
-    private connection;
-    constructor(database: string, migrations: Function);
-    getConnection(): Dexie;
-}
--- dist/sessionModel.js
+++
@@ -1,7 +0,0 @@
-import model from "./Model";
-export default new (class Session extends model {
-    constructor() {
-        super(false, ["key", "string"]);
-    }
-})();
-//# sourceMappingURL=sessionModel.js.map
