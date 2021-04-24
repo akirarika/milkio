0ceb2673bf0af750b3a19d1d3f9eef31f5547ebd
jctaoo <jctaoo@outlook.com>
2021-04-24 17:20:02 +0800

    setup english documents

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,42 +1,93 @@
 module.exports = {
   title: "Kurimudb",
-  description: "Just playing around.",
   head: [["link", { rel: "icon", href: "favicon.png" }]],
   markdown: {
     lineNumbers: true,
   },
+  locales: {
+    "/": {
+      lang: "zh-CN",
+      title: "Kurimudb",
+      description: "Just playing around.",
+    },
+    "/en/": {
+      lang: "en-US",
+      title: "Kurimudb",
+      description: "Just playing around.",
+    },
+  },
   themeConfig: {
-    search: true,
-    author: "akirarika",
-    nav: [
-      {
-        text: "更新日志",
-        link: "https://github.com/akirarika/kurimudb/releases",
-      },
-      { text: "Github", link: "https://github.com/akirarika/kurimudb" },
-    ],
-    sidebar: [
-      {
-        text: "文档",
-        children: [
-          { text: "入门", link: "/intro" },
-          { text: "模型", link: "/model" },
-          { text: "持久化", link: "/persistence" },
+    locales: {
+      "/": {
+        selectText: '选择语言',
+        label: '简体中文',
+        nav: [
           {
-            text: "驱动",
+            text: "更新日志",
+            link: "https://github.com/akirarika/kurimudb/releases",
+          },
+          { text: "Github", link: "https://github.com/akirarika/kurimudb" }
+        ],
+        sidebar: [
+          {
+            text: "文档",
             children: [
-              { text: "LocalStorage", link: "/drivers/localstorage" },
-              { text: "Cookie", link: "/drivers/cookie" },
-              { text: "Dexie (IndexedDB)", link: "/drivers/dexie" },
-              { text: "自定义驱动", link: "/drivers/custom-driver" },
+              { text: "入门", link: "/intro" },
+              { text: "模型", link: "/model" },
+              { text: "持久化", link: "/persistence" },
+              {
+                text: "驱动",
+                children: [
+                  { text: "LocalStorage", link: "/drivers/localstorage" },
+                  { text: "Cookie", link: "/drivers/cookie" },
+                  { text: "Dexie (IndexedDB)", link: "/drivers/dexie" },
+                  { text: "自定义驱动", link: "/drivers/custom-driver" },
+                ],
+              },
+              { text: "订阅变更", link: "/subscribe" },
+              { text: "TypeScript", link: "/typescript" },
             ],
           },
-          { text: "订阅变更", link: "/subscribe" },
-          { text: "TypeScript", link: "/typescript" },
+          { text: "最佳实践", link: "/bestPractices" },
+          { text: "模型生成器", link: "/modelCodeMaker" },
         ],
       },
-      { text: "最佳实践", link: "/bestPractices" },
-      { text: "模型生成器", link: "/modelCodeMaker" },
-    ],
+      "/en/": {
+        selectText: 'Languages',
+        label: 'English',
+        nav: [
+          {
+            text: "Changelog",
+            link: "https://github.com/akirarika/kurimudb/releases",
+          },
+          { text: "Github", link: "https://github.com/akirarika/kurimudb" }
+        ],
+        sidebar: [
+          {
+            text: "文档",
+            children: [
+              { text: "入门", link: "/intro" },
+              { text: "模型", link: "/model" },
+              { text: "持久化", link: "/persistence" },
+              {
+                text: "驱动",
+                children: [
+                  { text: "LocalStorage", link: "/drivers/localstorage" },
+                  { text: "Cookie", link: "/drivers/cookie" },
+                  { text: "Dexie (IndexedDB)", link: "/drivers/dexie" },
+                  { text: "自定义驱动", link: "/drivers/custom-driver" },
+                ],
+              },
+              { text: "订阅变更", link: "/subscribe" },
+              { text: "TypeScript", link: "/typescript" },
+            ],
+          },
+          { text: "最佳实践", link: "/bestPractices" },
+          { text: "模型生成器", link: "/modelCodeMaker" },
+        ],
+      },
+    },
+    search: true,
+    author: "akirarika",
   },
 };
