c9c13b328073c6dac85e39c46cc9c6037c7d5651
TheNightmareX <TheNightmareX@foxmail.com>
2021-08-21 22:44:28 +0800

    refactor(core): rename "PersistenceInterface" to `AbstractDriver` internally

--- packages/kurimudb/src/index.ts
+++ packages/kurimudb/src/index.ts
@@ -1,8 +1,8 @@
+export { AbstractDriver as PersistenceInterface } from "./abstract-driver.class";
 export { globalConfig as kurimudbConfig } from "./global-config";
 export { auto$ } from "./helpers/auto$.func";
 export { batch$ } from "./helpers/batch$.func";
 export { ModelOptionsInterface } from "./models/model-options.interface";
-export { PersistenceInterface } from "./persistence.interface";
 
 import { CollectionModel } from "./models/collection-model.class";
 import { KeyValueModel } from "./models/key-value-model.class";
