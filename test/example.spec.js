184d639164acac4fe4ad542698394303422a95a6
akirarika <i@xn--i2r.chat>
2021-08-27 02:03:56 +0800

    嘤嘤嘤

--- test/example.spec.js
+++ test/example.spec.js
@@ -1,5 +1,17 @@
-describe("Example test suite", () => {
-  test("must work", () => {
-    expect(42).toBe(42);
-  });
+import { SyncModels } from "kurimudb";
+
+class SyncState extends SyncModels.keyValue {
+  constructor() {
+    super({
+      name: "SyncKeyValueModel",
+    });
+  }
+}
+
+const syncState = new SyncState();
+
+test("adds 1 + 2 to equal 3", () => {
+  syncState.data.hello = "world";
+
+  expect(syncState.data.hello).toBe("world");
 });
