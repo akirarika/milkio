d88bb1298249334b580360314a14f85f95f1217d
akirarika <33272184+akirarika@users.noreply.github.com>
2024-06-29 19:28:32 +0800

    ✨ feat: let me release version 0.6.0

--- generated/raw/api-validator.ts
+++ generated/raw/api-validator.ts
@@ -3,11 +3,11 @@
  */
 
 export default {
-  generatedAt: 1719656060437,
+  generatedAt: 1719656277689,
   validate: {
-    'cookbook': () => import('./apps/cookbook.ts'),
     'foo': () => import('./apps/foo.ts'),
     'test': () => import('./apps/test.ts'),
+    'cookbook': () => import('./apps/cookbook.ts'),
     
   },
 }
