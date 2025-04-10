edc1b2368297bf22863b26c7a6b914315e8fe591
akirarika <33272184+akirarika@users.noreply.github.com>
2025-04-10 16:57:18 +0800

    ✨ feat: introduce a process exit function, add warning prompts for specific commands

--- packages/cookbook/src/commands/install.ts
+++ packages/cookbook/src/commands/install.ts
@@ -1,7 +1,8 @@
-import { cwd } from "node:process";
+import { cwd, exit } from "node:process";
 import { execScript } from "../utils/exec-script";
 import { defineCookbookCommand } from "@milkio/cookbook-command";
 import { selectProject } from "../utils/select-project";
+import consola from "consola";
 
 export default await defineCookbookCommand(async (utils) => {
   const cookbookToml = await utils.getCookbookToml();
@@ -11,6 +12,23 @@ export default await defineCookbookCommand(async (utils) => {
     const project = await selectProject(cookbookToml, {
       withRoot: true
     })
+    if (project.value !== "<root>" && (params.commands.find((command) => {
+      if (command === "milkio") return true
+      if (command.startsWith("@milkio/")) return true
+      if (command === "typescript") return true
+      if (command === "@biomejs/biome") return true
+      if (command === "typia") return true
+      if (command === "vitest") return true
+      if (command === "jest") return true
+      if (command === "eslint") return true
+      return false
+    }))) {
+      consola.warn("You seem to be installing milkio-related packages or some common public packages. We recommend that you install these packages in the root directory so that you can upgrade all monorepo packages uniformly. Otherwise, it may be painful for you to upgrade these public package versions for each monorepo in the future.")
+      if (!(await consola.prompt(`🐱 Now, do you still want to install these packages in ${project.value} instead of the root directory?`, { type: "confirm" }))) {
+        consola.success("Installation cancelled. You can choose the location <root> during installation.")
+        exit(0)
+      }
+    }
     path = project.path
   }
   const command = `${cookbookToml.general.packageManager} install ${params.raw.join(" ")}`;
