d20bcb8cdde9cb9fea87722766078b95132fbc3d
akirarika <33272184+akirarika@users.noreply.github.com>
2025-07-19 19:40:00 +0800

    ✨ feat: config checks

--- packages/cookbook/src/commands/build.ts
+++ packages/cookbook/src/commands/build.ts
@@ -1,16 +1,25 @@
 import { defineCookbookCommand } from "@milkio/cookbook-command";
 import { getCookbookToml } from "../utils/get-cookbook-toml";
 import { join } from "node:path";
-import { cwd } from "node:process";
+import { cwd, exit } from "node:process";
 import consola from "consola";
 import { execScript } from "../utils/exec-script";
 import { selectMode } from "../utils/select-mode";
 import { progress } from "../progress";
+import { calcHash } from "../utils/calc-hash";
 
 export default await defineCookbookCommand(async (utils) => {
-  const options = await getCookbookToml();
-  const params = utils.getParams();
+  const cookbookToml = Bun.file(join(cwd(), "cookbook.toml"));
+  if (!(await cookbookToml.exists())) {
+    consola.error(`The "cookbook.toml" file does not exist in the current directory: ${join(cwd())}`);
+    exit(0);
+  }
+  const cookbookTomlText = await cookbookToml.text();
+  const cookbookTomlHash = calcHash(cookbookTomlText);
+  const options = await getCookbookToml(cookbookTomlText, progress);
+  options.hash = cookbookTomlHash;
 
+  const params = utils.getParams();
   const mode = await selectMode(options);
 
   progress.open("cookbook building..");
--- packages/cookbook/src/watcher/extensions/config.ts
+++ packages/cookbook/src/watcher/extensions/config.ts
@@ -14,6 +14,7 @@ export const configWatcherExtension = defineWatcherExtension({
     typescriptExports += "\n\nexport const configSchema = { get: async () => {\n  return { mode,";
 
     for await (const file of allFiles) {
+      if (file.fileName !== `${mode}.config.ts`) continue;
       typescriptImports += `\nimport ${file.importName} from "../app/${file.path}";`;
       typescriptExports += `\n    // @ts-ignore\n    ...(await ${file.importName}(mode)),`;
     }
