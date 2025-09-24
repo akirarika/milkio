790858dd38cdb4aae387b8dda80a8a92bb9e4634
akirarika <33272184+akirarika@users.noreply.github.com>
2025-09-24 21:34:35 +0800

    🧊 fix: meta type handling

--- packages/milkio/action/index.ts
+++ packages/milkio/action/index.ts
@@ -3,7 +3,7 @@ import type { $context, $meta } from "../index.ts";
 export function action<ActionInitT extends ActionInit>(init: ActionInitT): Action<ActionInitT> {
     const action = init as unknown as Action<ActionInitT>;
     action.$milkioType = "action";
-    if (action.meta === undefined) action.meta = {};
+    if ((action as any).meta === undefined) (action as any).meta = {};
     return action;
 }
 
@@ -14,7 +14,6 @@ export type ActionInit = {
 
 export type Action<ActionInitT extends ActionInit> = {
     $milkioType: "action";
-    // meta: ActionInitT["meta"] extends undefined ? {} : ActionInitT["meta"];
-    meta: any;  // There is a bug in Typia. When meta exists and contains internally auto-inferred complex types, the entire action will be inferred as any, resulting in the failure of type checking.
+    // meta: ActionInitT["meta"] extends undefined ? {} : ActionInitT["meta"];  // There is a bug in Typia. When meta exists and contains internally auto-inferred complex types, the entire action will be inferred as any, resulting in the failure of type checking.
     handler: ActionInitT["handler"];
 };
