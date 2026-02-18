c023f19d56655cb69d7c983230130ab04b71ead1
akirarika <33272184+akirarika@users.noreply.github.com>
2026-02-18 18:20:38 +0800

    update

--- packages/milkio-redis/index.ts
+++ packages/milkio-redis/index.ts
@@ -311,27 +311,30 @@ export async function createRedis<RedisT extends { PSETEX: any; SET: any;[others
     return milkioRedis;
 }
 
-function reviveJSONParse<T>(json: T): T {
-    const isoDatePattern = /^(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d{1,3})?)(Z|[+-]\d{2}:?\d{2})?$/;
+const isoDatePattern = /^(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d{1,3})?)(Z|[+-]\d{2}:?\d{2})?$/;
 
-    if (json instanceof Date) return json;
+export function reviveJSONParse<T>(json: T): T {
+  if (json !== null && typeof json === 'object') {
+    if (typeof (json as any).getTime === 'function' && typeof (json as any).toISOString === 'function') {
+      return json;
+    }
     if (Array.isArray(json)) {
-        return json.map((item) => reviveJSONParse(item)) as any;
+      return json.map((item) => reviveJSONParse(item)) as any;
     }
-    if (typeof json === "object" && json !== null) {
-        return Object.entries(json).reduce((acc, [key, value]) => {
-            acc[key as keyof T] = reviveJSONParse(value);
-            return acc;
-        }, {} as T);
+    return Object.entries(json).reduce((acc, [key, value]) => {
+      acc[key as keyof T] = reviveJSONParse(value);
+      return acc;
+    }, {} as T);
+  }
+  if (typeof json === 'string') {
+    const match = json.match(isoDatePattern);
+    if (match) {
+      const normalizedDateString = match[2] ? `${match[1]}${match[2].replace(':', '')}` : `${match[1]}Z`;
+      return new Date(normalizedDateString) as any;
     }
-    if (typeof json === "string") {
-        const match = json.match(isoDatePattern);
-        if (match) {
-            const normalizedDateString = match[2] ? `${match[1]}${match[2].replace(":", "")}` : `${match[1]}Z`;
-            return new Date(normalizedDateString) as any;
-        }
-    }
-    return json;
+  }
+  return json;
 }
+
 
 export type Redis = MilkioRedis;
